4ab3207e2a9fc9541436efeed3d5e0a3c7b22576
rohit <rohit.selvakumaran@gmail.com>
2024-11-05 03:00:21 +0530

    training phase 1

---
+++ emoji_analysis/emoji_sentiment.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model 
+from sklearn.preprocessing import StandardScaler
+
+model = load_model('C:\\Rohit\\Projects\Fintech\\sentiment_analysis_for_business\\emoji_analysis\\emoji_sentiment_model.h5')
+
+def load_emoji_data(csv_file):
+    df = pd.read_csv(csv_file)
+    return df['Emoji'].tolist(), df
+
+def preprocess_data(emoji_sentiment_df):
+    
+    X = emoji_sentiment_df[['Negative', 'Neutral', 'Positive']].values
+    
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    return X_scaled
+
+
+def analyze_emoji_sentiment(extracted_emojis, emoji_sentiment_data):
+    positive_count = 0
+    negative_count = 0
+    neutral_count = 0
+
+    
+    for emoji in extracted_emojis:
+        
+        if emoji in emoji_sentiment_data['Emoji'].values:
+            sentiment_data = emoji_sentiment_data[emoji_sentiment_data['Emoji'] == emoji].iloc[0]
+            negative = sentiment_data['Negative']
+            neutral = sentiment_data['Neutral']
+            positive = sentiment_data['Positive']
+
+            
+            if positive > negative and positive > neutral:
+                positive_count += 1
+            elif negative > positive and negative > neutral:
+                negative_count += 1
+            else:
+                neutral_count += 1
+
+
+    if positive_count > negative_count:
+        return "Positive"
+    elif negative_count > positive_count:
+        return "Negative"
+    else:
+        return "Neutral"
+
+
---
+++ emoji_analysis/finbert_analysis.py
@@ -0,0 +1,32 @@
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+
+model_name = "ProsusAI/finBERT"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name)
+
+def analyze_sentiment(statement):
+    inputs = tokenizer(statement, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+        
+    predicted_class = torch.argmax(outputs.logits, dim=1).item()
+    
+    sentiment_map = {
+        0: "Negative",
+        1: "Neutral",
+        2: "Positive"
+    }
+    
+    sentiment = sentiment_map.get(predicted_class, "Unknown")
+    return sentiment
+
+def get_recommendation(sentiment):
+    if sentiment == "Positive":
+        return "Share this positive news with your followers to boost confidence and engagement!"
+    elif sentiment == "Negative":
+        return "Consider addressing concerns transparently to maintain trust among your stakeholders."
+    else:
+        return "Try to improve your current strategy while monitoring the situation; clarity is key."
